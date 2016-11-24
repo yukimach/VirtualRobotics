16a9997928ca0a96aa5490ef438be6d4a1786131
Yuki Machida <yuki.machida@epfl.ch>
2016-11-25 00:30:21 +0100

    Walls + Roomba // Move forward // print x, z directions //

---
+++ roomba/Assets/Scripts/Move.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Move : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.Translate (Vector3.forward * Time.deltaTime);
+		print (transform.position.x + "," + transform.position.z);
+	}
+}
